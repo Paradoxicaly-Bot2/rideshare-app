8fe4b7385afcc42a82225ab14c4e704f5e0f79d6
Derrick K <derrick.kwan.360@gmail.com>
2024-08-21 21:04:23 -0700

    Launch

--- carpool/settings.py
+++ carpool/settings.py
@@ -18,7 +18,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://monkfish-app-vwufh.ondigitalocean.app',
+    'https://whale-app-k98xh.ondigitalocean.app/',
     'https://camsconnects.com',
     
     'https://yourdomain.com',
