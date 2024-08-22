e25234385f217977c0550d6501e16d74086d6522
Derrick K <derrick.kwan.360@gmail.com>
2024-08-21 22:17:17 -0700

    Update settings.py

--- carpool/settings.py
+++ carpool/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'op7*^-7$x#)y9&^3ode$a#ane+7ee!r3o9qphftj#m&1x3xdqy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
